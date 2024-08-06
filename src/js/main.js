7d3368cc2a7cc27c4e728c5663ad5b4aa99674bd
Fcaspirro <fabio_caspirro@hotmail.com>
2024-08-06 20:16:30 -0300

    refactor: video and audio behavior

--- src/js/main.js
+++ src/js/main.js
@@ -1,45 +1,30 @@
 $(function() {
-  const $owlCarousel = $('.owl-carousel');
-  const $soundButtons = $('.sound');
-  
-  // Inicializa o carrossel
-  $owlCarousel.owlCarousel({
+  // Carousel
+  $('.owl-carousel').owlCarousel({
     loop: false,
     items: 1,
     dots: false,
     nav: true,
     navText: ["<i class='bi bi-arrow-left'></i>", "<i class='bi bi-arrow-right'></i>"],
-    onChange: function() {
-      // Pausa e muta o vídeo do slide atual
-      $('.owl-item.active video').each(function() {
-        const $video = $(this);
-        $video.get(0).pause();
-        $video.prop('muted', true);
-      });
-
-      // Reseta os botões de áudio dos slides não ativos
-      $('.owl-item').not('.active').each(function() {
-        const $slide = $(this);
-        $slide.find('.sound-on').hide();
-        $slide.find('.sound-muted').show();
-      });
-    },
-    onTranslated: function() {
-      // Retoma o vídeo do slide ativo
-      $('.owl-item.active video').each(function() {
-        $(this).get(0).play();
-      });
-    }
   });
 
-  // Alterna o áudio dos vídeos
-  $soundButtons.on('click', function() {
-    const $this = $(this);
-    const $video = $this.siblings('video');
-    const isMuted = $this.hasClass('sound-muted');
+  var $audio = $('#song')[0];
+  
+  if ($audio) {
+    $audio.volume = 0.05; 
     
-    $video.prop('muted', !isMuted);
-    $this.toggle();
-    $this.siblings('.sound').toggle();
-  });
+    $(".sound-on").hide();
+
+    $(".sound-muted").on('click', function() {
+      $(".sound-on").fadeIn();
+      $(".sound-muted").fadeOut();
+      $audio.play(); 
+    });
+
+    $(".sound-on").on('click', function() {
+      $(".sound-muted").fadeIn();
+      $(".sound-on").fadeOut();
+      $audio.pause(); 
+    });
+  }
 });
