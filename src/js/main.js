d2ff02cb8d21489b637049f7087b3d63a99513e8
Fcaspirro <fabio_caspirro@hotmail.com>
2024-08-14 19:20:26 -0300

    fix: import js modules

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
-import { initHeroCarousel, initGalleryCarousel } from './components/Carousel/index.module.js';
-import { initAudio } from './components/Audio/index.module.js';
-import { initInfoButton, initBackButton } from './components/Buttons/index.module.js';
-import { initGallery } from './components/Gallery/index.module.js';
+import { initHeroCarousel, initGalleryCarousel } from '../../src/js/components/Carousel/index.module.js';
+import { initAudio } from '../../src/js/components/Audio/index.module.js';
+import { initInfoButton, initBackButton } from '../../src/js/components/Buttons/index.module.js';
+import { initGallery } from '../../src/js/components/Gallery/index.module.js';
 
 $(function() {
   initHeroCarousel();
